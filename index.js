9e489a03fcea949ac3e249e09b94044d55401a17
Frasia <108404961+SFrasia@users.noreply.github.com>
2024-01-16 18:34:05 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution here
+const drivers = ["Bobby", "Sammy", "Sally", "Annette", "Sarah", "Bobby"];
+
+const findMatching = drivers.find((element) => element === "Bobby");
+
+const findSusan = drivers.find(function (element) {
+  return element === "Susan";
+});
+
+const Alldrivers = drivers(drivers.length);
+
+const firstDriver = drivers.find(function (driver) {
+  return driver.startsWith("Sa");
+});
+
+const matchDriver = drivers.find(function (driver) {
+  return driver.startsWith("y");
+});
+
+const middleEndDiver = drivers.find(function (driver) {
+  return driver.startsWith("mm");
+});
+
+const result = drivers.find(({ name }) => name === "Bobby");
+// Code your solution here
